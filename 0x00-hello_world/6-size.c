f9a129bd14be4a9133935a9b8748394c9db3ca6c
jdcera4 <jdcera4@gmail.com>
2021-02-04 05:55:14 +0000

    punto6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Size of a char: %lu",sizeof(char));
+  printf(" byte(s)\n");
+  printf("Size of a int: %lu",sizeof(int));
+  printf(" byte(s)\n");
+  printf("Size of a long: %lu",sizeof(long));
+  printf(" byte(s)\n");
+  printf("Size of a lon long int: %lu",sizeof(long int));
+  printf(" byte(s)\n");
+  printf("Size o a float: %lu",sizeof(float));
+  printf(" byte(s)\nx");
+  return(0);
+}
