72a9958ed3c6c1aa4efa0c375892c1bb72f5b60c
jdcera4 <jdcera4@gmail.com>
2021-02-05 03:56:18 +0000

    punto7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point and execute tebahpla
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  char i;
+  
+  for (i = 'z'; i >= 'a'; i--)
+    putchar (i);
+  putchar ('\n');
+  return (0);
+}
