bfc52eddce416d9e8d0dc83916d39aacf11468ff
jdcera4 <jdcera4@gmail.com>
2021-02-05 00:08:48 +0000

    punto1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point and execute the ramdon number and print the last digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  int n;
+  int lt;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  lt = n % 10;
+
+  if (lt > 5)
+    {
+      printf ("Last digit of %d is %d and is greater than 5\n", n, lt);
+    }
+  else if (lt == 0)
+    {
+      printf ("Last digit of %d is %d and is 0\n", n, lt);
+    }
+  else
+    {
+      printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, lt);
+    }
+  return (0);
+}
