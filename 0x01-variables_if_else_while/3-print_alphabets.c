76f5ef8740f7008b29dcd827adb67a6743380cbd
jdcera4 <jdcera4@gmail.com>
2021-02-05 02:03:08 +0000

    punto3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point and execute the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  char i;
+  char a;
+
+  for (i = 'a'; i <= 'z'; ++i)
+    putchar(i);
+  for (a = 'A'; a <= 'Z'; ++a)
+    putchar(a);
+  putchar('\n');
+  return(0);
+}
