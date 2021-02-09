e7f1a258ea9f485a8981d3adcec51cf03f30166d
jdcera4 <jdcera4@gmail.com>
2021-02-09 22:56:40 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include "printalfa.h"
+/**
+ * main . Entry point fuction
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+  print_alphabet();
+  _putchar ('\n');
+  return (0);
+}
