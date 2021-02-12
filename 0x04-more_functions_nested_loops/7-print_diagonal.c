d6f74dade26e029207ab88b6ac69d85c54c76e16
jdcera4 <jdcera4@gmail.com>
2021-02-12 04:33:52 +0000

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: prints lines
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 1; a <= n; a++)
+		{
+			for (b = 1; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
