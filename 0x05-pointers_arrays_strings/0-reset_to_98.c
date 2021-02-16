a60e64524970fc7b665e88cad7002d69e3a509b2
jdcera4 <jdcera4@gmail.com>
2021-02-16 21:16:27 +0000

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-* reset_to_98 - updates the value it points to to 98
-*
-* @n: variable 
-*/
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
+ */
 
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * swap_int - swaps the
+ * @a: first int to swap
+ * @b: second int to swap
  *
- * 
+ * Return: void
  */
 
- void swap_int(int *a, int *b)
- {
+void swap_int(int *a, int *b)
+{
 	*a = 42;
 	*b = 98;
 }
