9c972619567428f5ecdc6097500f59be114e22a0
jdcera4 <jdcera4@gmail.com>
2021-02-18 23:56:35 +0000

    todo

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+
+
+
+
+void rev_string(char *s)
+{
+	// int c;
+	// char aux[10];
+
+	// for (c = 9; s[c] != '\0'; c--)
+	// {
+	// 	c--;	
+	// }
+	// c--;54
+	// for (; c >= 0;)
+	// {
+	// 	c--;
+		
+	// }
+}
