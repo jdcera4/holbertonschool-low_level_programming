925732f4785bfdcfa8831dee829f7eeb92ae1e4b
jdcera4 <jdcera4@gmail.com>
2021-02-23 03:52:10 +0000

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,24 +2,27 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * _memcpy - prints buffer in hexa
+ * @dest: the address of memory to print
+ * @src: the size of the memory to print
+ * @n: variable cant
  * Return: Nothing.
  */
 
- char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *cde = dest;
+
 	char *crc = src;
+
 	unsigned int c;
+
 	c = 0;
+
 	while (c < n)
 	{
 		cde[c] = crc[c];
 		c++;
-		
 	}
 	return (cde);
 }
