b5adeed7c131b80a26c2c9ae76d1f5497d19f863
jdcera4 <jdcera4@gmail.com>
2021-03-03 17:40:18 +0000

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _islower - Checks whether c is a lowercase letter
+ * _isalpha - Checks whether c is a lowercase letter
  * @c: letter to return
  * Return: void
  */
