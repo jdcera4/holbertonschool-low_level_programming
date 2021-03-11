9dbf401356483331daca28ffbd9bd58ffc049a69
jdcera4 <jdcera4@gmail.com>
2021-03-11 13:24:13 +0000

    h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 
 void print_name_uppercase(char *name);
 
 void print_name_as_is(char *name);
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
