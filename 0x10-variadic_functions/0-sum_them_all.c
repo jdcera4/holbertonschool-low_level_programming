7da174a15775aa90817b51a88698e7b91fe39f21
jdcera4 <jdcera4@gmail.com>
2021-03-13 02:28:22 +0000

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,22 +4,22 @@
 
 /**
  * sum_them_all - check the code for Holberton School students.
- *
+ * @n: cantidad parameters
  * Return: Always 0.
  */
 
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list ar;
-    unsigned int i;
-    int s;
+	va_list ar;
+	unsigned int i;
+	int s;
 
-    va_start (ar, n);
-    s = 0;
-    for (i = 0; i < n; i++)
-        s += va_arg (ar, int);
+	va_start(ar, n);
+	s = 0;
+	for (i = 0; i < n; i++)
+		s += va_arg(ar, int);
 
-    va_end (ar);
-    return (s);
+	va_end(ar);
+	return (s);
 }
