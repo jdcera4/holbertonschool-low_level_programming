53c63126806ffbb1ff08bc53f5245d5c8aad5e69
jdcera4 <jdcera4@gmail.com>
2021-03-22 02:36:01 +0000

    count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: int parameter
+ * @argv: char* parameter
+ *
+ * Return: punt or null.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+    int count = 0;
+
+      for (count = 1; count < argc; )
+	{
+        printf("%i\n", count);
+        count++;
+	}
+    
+
+  return 0;
+}
