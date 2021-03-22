3ea9d61350d9ec895b4b5a8d909e3fe0bf834da7
jdcera4 <jdcera4@gmail.com>
2021-03-22 01:34:24 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    int i = 0;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		putchar (a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
