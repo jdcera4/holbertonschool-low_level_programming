9a5ea3b43925b816ac7c52c37b8a12f6a1da8344
jdcera4 <jdcera4@gmail.com>
2021-03-23 17:54:22 +0000

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: int parameter
+ * @argv: char* parameter
+ *
+ * Return: punt or null.
+ */
+
+int main(int argc, char *argv[])
+{
+	int val1, val2, result;
+
+	if (argc != 3)
+	{
+		printf ("Error\n");
+		return (1);
+	}
+	else
+	{
+		val1 = atoi(argv[1]);
+		val2 = atoi(argv[2]);
+		result = val1 * val2;
+		printf("%d\n", result);
+	}
+	return (0);
+}
