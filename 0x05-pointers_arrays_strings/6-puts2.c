22ad07eb5dd98bb9a3ef94543b2e91a37a3206f1
jdcera4 <jdcera4@gmail.com>
2021-03-24 01:39:39 +0000

    betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * rev_string - check the code for Holberton School students.
- * @s: a string
+ * puts2 - check the code for Holberton School students.
+ * @str: a string
  * Return: Always 0.
  */
 
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i * 2]);
-    }
-    _putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i * 2]);
+	}
+	_putchar('\n');
 
-	
+
 }
