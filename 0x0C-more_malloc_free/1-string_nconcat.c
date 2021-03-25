f377e07817982641fe932fd08ba80bf0dfc5fa6d
jdcera4 <jdcera4@gmail.com>
2021-03-25 14:15:05 +0000

    betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-
 	for (j = 0; s2[i] != '\0'; j++)
 	{
 	}
@@ -47,8 +46,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s2[l];
 	}
-
 	p[i] = '\0';
-
 	return (p);
 }
