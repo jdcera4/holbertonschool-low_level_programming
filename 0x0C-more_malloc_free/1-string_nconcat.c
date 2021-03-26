66ba4e5720cffdc523b37337f907b7732e45a0d6
jdcera4 <jdcera4@gmail.com>
2021-03-26 22:32:34 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,13 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i, j, l;
+	unsigned int i, j, l, aux;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
@@ -27,10 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-	j++;
 	if (n < j)
 		j = n;
-
+	aux = j;
 	j += i;
 	p = malloc(sizeof(char) * j + 1);
 
@@ -42,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
 
-	for (l = 0; l < n; l++, i++)
+	for (l = 0; l < aux; l++, i++)
 	{
 		p[i] = s2[l];
 	}
