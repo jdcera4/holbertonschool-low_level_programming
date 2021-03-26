9f6dec0f23ccc51dae55acff2c6f37de33cef18d
jdcera4 <jdcera4@gmail.com>
2021-03-26 03:48:16 +0000

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct dog - check the code for Holberton School students.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+
 typedef struct dog
 {
 	char *name;
