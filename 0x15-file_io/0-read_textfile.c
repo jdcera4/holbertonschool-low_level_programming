33c9584106d47c23d2a6612ef9fde485e3ff9c30
jdcera4 <jdcera4@gmail.com>
2021-03-30 00:17:51 +0000

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (NULL);
 	}
 
-	file = open(filename, O_DONLY);
+	file = open(filename, O_RDONLY);
 	if (file < 0)
 	{
 		return (0);
