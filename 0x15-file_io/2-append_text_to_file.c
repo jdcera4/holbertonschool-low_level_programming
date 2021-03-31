c3a6abcc8384fa54d212e2a84560012098c7512e
jdcera4 <jdcera4@gmail.com>
2021-03-31 02:52:46 +0000

    betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -41,19 +41,19 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content != NULL)
-    {
-        lon = _strlen(text_content);
-    }
-    else
-    {
-        return (-1);
-    }
+	{
+		lon = _strlen(text_content);
+	}
+	else
+	{
+		return (-1);
+	}
 
-    print = write(oupen, text_content, lon);
-    if (print == NULL)
-    {
-        return (-1);
-    }
-    
+	print = write(oupen, text_content, lon);
+	if (print == NULL)
+	{
+		return (-1);
+	}
+
 	return (1);
 }
