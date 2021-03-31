7b3056b444d3c2292597e7c7e39da9f36bae6717
jdcera4 <jdcera4@gmail.com>
2021-03-31 02:57:44 +0000

    arreglo 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int _strlen(char *s)
  * Return: Always 0.
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int oupen, print, lon;
 
