a8c2e2e8c982682a6429a2bfede8ddcf5c1b3eac
jdcera4 <jdcera4@gmail.com>
2021-04-15 21:53:15 -0500

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,3 +46,21 @@ void open_and_read(char *file_from, char *file_to)
 		}
 	}
 }
+
+/**
+ * main - entry point.
+ * @argc: the number of command line arguments.
+ * @argv: An array of size argc.
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char **argv)
+{
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open_and_read(argv[1], argv[2]);
+	return (0);
+}
