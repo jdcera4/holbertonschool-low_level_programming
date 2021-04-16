0fb46778ab3db91e17b74dfa3248da78285092ab
jdcera4 <jdcera4@gmail.com>
2021-04-16 00:45:01 +0000

    7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -39,16 +39,8 @@ int palindrome_compare(char *s, int i, int c)
  */
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[i] == '\0')
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	if (s[i] != '\0')
-	{
-		return (1 + longitud_cadena(&s[i + 1]));
-	}
-	return (0);
+
+	return (1 + _strlen_recursion(s + 1));
 }
