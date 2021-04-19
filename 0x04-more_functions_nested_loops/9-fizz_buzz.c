4d41486cc5f1867decf855322690114aef88e0c5
jdcera4 <jdcera4@gmail.com>
2021-04-18 21:04:51 -0500

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i = 1;
+
+	while(i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+			
+		
+		
+		
+		i++;
+	}
+}
