30e4d8a687be060084406f6fc5be379771103498
jdcera4 <jdcera4@gmail.com>
2021-07-04 17:27:26 -0500

    return pereimeter of grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" grid """
+
+
+def island_perimeter(grid):
+    """ """
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row -1 < 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                if column - 1 < 0 or grid[row][column - 1] == 0:
+                    perimeter += 1
+                if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
+                    perimeter += 1
+                if row + 1 >= len(grid) or grid[row + 1][column] == 0:
+                    perimeter += 1
+    return perimeter
